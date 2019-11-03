69d459ee6134bb138808aed5ed8323fae6d216c6
Richard <r.banks@burnley.ac.uk>
2019-11-03 22:14:40 +0000

    Added some more texture maps

--- Assets/Scripts/Traps/DropTrap.cs
+++ Assets/Scripts/Traps/DropTrap.cs
@@ -18,19 +18,20 @@ public class DropTrap : MonoBehaviour
 	// Use this for initialization
 	void Awake ()
     {
-        anim = GetComponent<Animator>();
-	    //StartCoroutine(AutoTrap());
+        anim = GetComponentInChildren<Animator>();
+	    StartCoroutine(AutoTrap());
 
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
-        if (Input.GetKeyDown(KeyCode.S))
+       /* if (Input.GetKeyDown(KeyCode.S))
         {
 
             anim.SetTrigger("Trap");
         }
+        */ 
 	}
 
 	IEnumerator AutoTrap()
